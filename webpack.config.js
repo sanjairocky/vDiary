6bbb94c8e8f461368dbb2484a4f471cea230c56f
sanjairocky <talk2sanjai@gmail.com>
2022-03-07 10:32:51 +0530

    Create deploy.yml

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     filename: "js/[name].bundle.js",
     path: path.resolve(__dirname, "dist"), // base path where to send compiled assets
-    publicPath: "/", // base path where referenced files will be look for
+    publicPath: "", // base path where referenced files will be look for
     pathinfo: false,
   },
   resolve: {
